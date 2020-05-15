f8062717126ab97fd662a4eb02826749200b28c2
Vaid Saraswat <vaidsaraswat@gmail.com>
2020-05-15 13:50:08 -0400

    updated content

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const sendMail = require('./mail');
+const sendMail = require('./mail.js');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = 5000;
 
-//Bring CSS and Images
 app.use('/Images', express.static('Images'));
 app.use('/CSS', express.static('CSS'));
 
@@ -49,6 +48,10 @@ app.get('/contact', (req, res)=>{
   res.sendFile(path.join(__dirname,'contact.html'));
 });
 
+app.get('/faq', (req, res)=>{
+  res.sendFile(path.join(__dirname,'faq.html'));
+});
+
 //data parsing
 app.use(express.urlencoded({
   extended: false
@@ -58,16 +61,15 @@ app.use(express.json());
 
 app.post('/email',(req,res)=>{
   //send email here
-  console.log('Data: ', req.body);
   const {email, message, date, time} = req.body;
 
   sendMail(email, message, date, time, (err, data)=>{
     if(err){
       res.status(500).json({message: 'Internal Error'});
     }
-    res.json({message: 'Email sent!!!!'});
+    //res.json({message: 'Email sent!!!!'});
   });
-  res.json({message: 'Message received!'});
+  //res.json({message: 'Message received!'});
 });
 
 app.listen(PORT, ()=>{
--- mail.js
+++ mail.js
@@ -1,13 +1,13 @@
-var API_KEY = '';
-var DOMAIN = '';
+var API_KEY = 'key-5dae48591f5883bbbde3119b78e00ce5';
+var DOMAIN = 'mg.peelgroupbenefits.ca';
 var mailgun = require('mailgun-js')({apiKey: API_KEY, domain: DOMAIN});
 
 const sendMail = (email, message, date, time)=> {
   const mailOptions = {
     from: email,
-    to: "",
+    to: "vikassaraswat@rogers.com",
     subject: 'Appointment Confirmation',
-    text: `Meeting Details: \n Date: ${date} \n Time: ${time} \n Location:  \n Additional Notes: ${message}`
+    text: `Meeting Details: \n Date: ${date} \n Time: ${time} \n Location: 5705 Cancross Court Suite 200, Mississauga Ontario - L5R 3E9 \n Additional Notes: ${message}`
   };
 
   mailgun.messages().send(mailOptions, (error, body) => {
@@ -15,10 +15,10 @@ const sendMail = (email, message, date, time)=> {
     {
       console.log(error);
     }
-    else
+    /*else
     {
       console.log(body);
-    }
+    }*/
   });
 }
 
